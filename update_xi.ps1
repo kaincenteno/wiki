c2e2dfe609ced87178b204330e15b0137da3faf5
Administrator <kain.centeno@catsangel.com>
2022-03-23 03:56:00 +0000

    docs: upload update_xi.ps1

---
+++ update_xi.ps1
@@ -0,0 +1,17 @@
+﻿#Requires -RunAsAdministrator
+
+$game_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\PlayOnlineUS\InstallFolder').'0001'
+$pol_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\PlayOnlineUS\InstallFolder').'1000'
+$pol_path = "${pol_path}\pol.exe"
+
+$file_txt = @'
+123456:654321:FTABLE.DAT
+::
+
+'@
+
+Remove-Item "${game_path}FTABLE.DAT"
+Remove-Item "${game_path}file.txt"
+New-Item -Path $game_path -Name 'file.txt' -ItemType "file" -Value $file_txt
+
+Start-Process -wait $pol_path
